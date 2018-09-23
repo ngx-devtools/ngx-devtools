b0c0f91bc33d7bae069c4a04847831013ff44b7d
aelbore <aelbore.sg@gmail.com>
2018-09-23 20:42:22 +0800

    add scripts

--- index.ts
+++ index.ts
@@ -1,14 +1,29 @@
+
+import { symlink, unlink } from 'fs';
+import { promisify } from 'util';
+import { join, resolve } from 'path';
+
 const gitAsync = require('simple-git/promise');
+const exec =  require('child_process').exec;
+
+const symlinkAsync = promisify(symlink);
+const unlinkAsync = promisify(unlink);
 
 (async function() {
-  const packages = [ 'common', 'build', 'server', 'task' ]
+  const packages = [ 'common', 'build', 'server', 'task' ];
 
-  Promise.all(packages.map(package => {
-    const pkg = gitAsync(`./packages/${package}`);
+  Promise.all(packages.map(folder => {
+    const pkgFolder = `packages/${folder}`;
+    const pkg = gitAsync(`./${pkgFolder}`);
     return pkg.checkout('master')
-      .then(() => pkg.pull())
-      .then(() => pkg.checkout('ts'));
-  }))
-
-
+      .then(() => exec(`cd ${pkgFolder}`))
+      .then(() => exec(`git pull origin master`))
+      .then(() => pkg.checkout('ts'))
+      .then(() => {
+        const src = join(resolve(), 'node_modules');
+        const dest = join(resolve(), pkgFolder, 'node_modules');
+        return symlinkAsync(src, dest, 'dir')
+      })
+  }));
+  
 })();
