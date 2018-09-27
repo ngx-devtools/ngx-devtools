2143bb6a7981d85a91c0abc988d054fa135d4d56
aelbore <aelbore.sg@gmail.com>
2018-09-27 09:19:48 +0800

    add utils/tools scripts

--- index.ts
+++ index.ts
@@ -1,58 +1,18 @@
 
-import { symlink, unlink, lstat, readdir, rmdir, existsSync, readlinkSync, statSync, mkdirSync } from 'fs';
+import { symlink } from 'fs';
 import { promisify } from 'util';
-import { join, resolve, sep, dirname } from 'path';
+import { join, resolve } from 'path';
+
+import { clean } from './tools/clean';
+import { symlinkPackages } from './tools/link-packages';
 
 const gitAsync = require('simple-git/promise');
 const exec =  require('child_process').exec;
 
 const symlinkAsync = promisify(symlink);
-const unlinkAsync = promisify(unlink);
-const lstatAsync = promisify(lstat);
-const readdirAsync = promisify(readdir);
-const rmdirAsync = promisify(rmdir);
 
 (async function() {
   const packages = [ 'common', 'build', 'server', 'task' ];
-
-  function mkdirp(directory: string) {
-    const dirPath = resolve(directory).replace(/\/$/, '').split(sep);
-    for (let i = 1; i <= dirPath.length; i++) {
-      const segment = dirPath.slice(0, i).join(sep);
-      if (!existsSync(segment) && segment.length > 0) {
-        mkdirSync(segment);
-      }
-    }
-  }
-
-  async function clean(dir: string) {
-    if (existsSync(dir)) {
-      const files = await readdirAsync(dir);
-      await Promise.all(files.map(async (file) => {
-        const p = join(dir, file);
-        const stat = await lstatAsync(p);
-        if (stat.isDirectory()) {
-          await clean(p);
-        } else {
-          await unlinkAsync(p);
-        }
-      }));
-      await rmdirAsync(dir);
-    }
-  }
-
-  async function symlinkPackages(src, dest) {
-    const srcNodeModules = join(src, 'node_modules');
-    return ((existsSync(srcNodeModules) && statSync(srcNodeModules).isDirectory())
-      ? readlinkSync(srcNodeModules) === join(resolve(), 'node_modules')
-        ? unlinkAsync(srcNodeModules)
-        : clean(srcNodeModules)
-      : Promise.resolve()
-    ).then(() => {
-      mkdirp(dirname(dest))
-      return symlinkAsync(src, dest, 'dir');
-    })
-  }
 
   await clean('node_modules/@ngx-devtools');
 
