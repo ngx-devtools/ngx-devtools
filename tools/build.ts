639406dc43d8354a6a45ce7eed489e6c0ad523c7
aelbore <aelbore.sg@gmail.com>
2018-09-28 23:20:24 +0800

    updates

--- tools/build.ts
+++ tools/build.ts
@@ -1,8 +1,18 @@
+import { createRollupConfig, ngxBuild  } from '../packages/common/src/build-package';
 import { clean } from './clean';
 
-const exec =  require('child_process').exec;
+import { globFiles, copyFileAsync } from '../packages/common/src/file';
+import { extname, join, resolve, sep } from 'path';
+import { mkdirp } from './mkdirp';
 
 const packages = [ 'common', 'build', 'server', 'task' ];
+
+async function copyFiles(files) {
+  return Promise.all(files.map(file => {
+    const destPath = file.replace('src', '.tmp');
+    return copyFileAsync(file, destPath);
+  }))
+}
 
 async function build() {
   await Promise.all(packages.map(folder => {
@@ -11,9 +21,20 @@ async function build() {
       clean(`packages/${folder}/.tmp`) 
     ])
   }))
+
+  // const filter = file => extname(file) === '.ts';
+  // const map = file => `export * from '${file.replace(join(resolve(), sep, 'src', sep), './').replace('.ts', '')}';`;
+  // const sourceFiles = files.filter(filter).map(map).join('\n');
+    
   for (const folder of packages) {
-    await exec(`npm run build --prefix packages/${folder}`);
+    const files = await globFiles(`packages/${folder}/src/**/*.*`);
+
+    const tmpFolder = join(resolve(), 'packages', folder, '.tmp');
+    await mkdirp(tmpFolder);
+
+    await copyFiles(files);
   }
+
 }
 
 export {  build }
